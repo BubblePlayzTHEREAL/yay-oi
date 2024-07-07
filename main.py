a653c151b0a318cf45868056bdcec962407c901a
BubblePlayz <58344347+BubblePlayzTHEREAL@users.noreply.github.com>
2024-07-07 18:50:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ model = genai.GenerativeModel(
   generation_config=generation_config,
 )
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def root():
     data = request.json
     user_input = data.get('txt', '')
