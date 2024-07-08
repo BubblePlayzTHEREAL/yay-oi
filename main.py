4b0e5f37f0d697d244866bed9052233cd14159df
BubblePlayz <58344347+BubblePlayzTHEREAL@users.noreply.github.com>
2024-07-07 19:13:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import os
 import google.generativeai as genai
 
@@ -7,16 +7,16 @@ app = Flask(__name__)
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 64,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
 }
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash",
-  generation_config=generation_config,
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
 )
 
 @app.route('/', methods=['POST'])
@@ -24,13 +24,10 @@ def root():
     data = request.json
     user_input = data.get('txt', '')
     
-    chat_session = model.start_chat(
-      history=[]
-    )
-    
+    chat_session = model.start_chat(history=[])
     response = chat_session.send_message(user_input)
     
     return response.text
-  
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
