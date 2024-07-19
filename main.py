365327a0c6f0ad5f8e58ff212eacdac338449084
BubblePlayz <58344347+BubblePlayzTHEREAL@users.noreply.github.com>
2024-07-18 19:42:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,23 @@ generation_config = {
     "response_mime_type": "text/plain",
 }
 
-model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
-    generation_config=generation_config,
-)
+
 
 @app.route('/', methods=['POST','GET'])
 def root():
     data = request.get_json()
     user_input = data.get('txt')
+    image = data.get('image', None)
+    history = data.get('history', [])
+    rules = data.get('rules')
     
-    chat_session = model.start_chat(history=[])
+    model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+    system_instruction=rules
+    )
+    
+    chat_session = model.start_chat(history=history)
     try:
         response = chat_session.send_message(user_input)
         return response.text
